873ca6f9b037d51045aa0810dcb3194e3e544c5a
Batsukh <batsukh@Univions7602.local>
2026-08-21 01:25:02 +0800

    Initial commit

--- server2.js
+++ server2.js
@@ -1,10 +1,22 @@
 // Importing express itself into the server
 const express = require('express');
+const Database = require('better-sqlite3');
 
 // Calling express creates an app
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
+
+//Creating the database file
+const db = new Database('blog.db'); 
+
+db.exec(`
+  CREATE TABLE IF NOT EXISTS posts (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
+  )
+`);
+
 
 app.get('/', (req, res) => {
   res.send(`
@@ -18,10 +30,28 @@ app.get('/', (req, res) => {
 });
 
 app.post('/posts', (req, res) => {
-  console.log(req.body)
+  const { title, content } = req.body;
 
-  res.send(`Recieved title: ${req.body.title}, Content: ${req.body.content}`);
+  const stmt = db.prepare('INSERT INTO posts (title, content) VALUES (?, ?)');
+  stmt.run(title, content);
+
+  res.send(`Saved! Title: ${title}`);
+
+
 });
+
+app.get('/posts', (req, res) => {
+  const posts = db.prepare('SELECT * FROM posts').all();
+
+  const html = posts.map(post => 
+    `<h2>${post.title}</h2>
+    <p>${post.content}</p>
+    <hr/>`
+    ).join('');
+
+    res.send(html)
+});
+
 
 app.get('/about', (req, res) => {
   res.send('About page')
